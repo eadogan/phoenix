3dde39a380c8e9203646af2af87ce7b20025f4f5
eadogan <e.ahmetdogan@gmail.com>
2017-09-30 19:29:40 +0100

    Ignore updated and DB created

---
+++ src/main/java/NoteCalculate/NoteCalculates/Database.java
@@ -0,0 +1,13 @@
+package NoteCalculate.NoteCalculates;
+
+
+
+public interface Database {
+	
+	//Normal Note Calculate
+	public boolean calculateNormally(Student student);
+	
+	//Average Note Calculate
+	public boolean calculateAverage(Student student);
+
+}
